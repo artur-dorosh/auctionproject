7e783b8709d5d006766b87b9576f926f84bd7303
artur-dorosh <50192515+artur-dorosh@users.noreply.github.com>
2019-05-02 19:59:25 +0300

    предпоследний по верстке

--- js/js.js
+++ js/js.js
@@ -2,7 +2,7 @@
 
 function initMap() {
     var location = {lat: 50.4486136, lng: 30.4564757};
-    var map = new google.maps.Map(document.getElementById('map'), {
+    var map = new google.maps.Map(document.getElementById("map"), {
       center: location,
       zoom: 17,
       disableDefaultUI: true
@@ -12,4 +12,37 @@ function initMap() {
         map: map,
         animation: google.maps.Animation.DROP
     });
+}
+
+
+var anchorLinks = document.querySelectorAll("[href^=\"#\"]");
+
+/* scroll */
+/*
+var scrolled, timer;
+
+document.getElementById("toTop").onclick = function() {
+    scrolled = window.pageYOffset;
+    scrollToTop();
+}
+
+function scrollToTop() {
+    if (scrolled > 0) {
+        window.scrollTo(0, scrolled);
+        scrolled -= 20;
+        timer = setTimeout(scrollToTop, 10);
+    } else {
+        clearTimeout(timer);
+    }
+}
+*/
+/* toTop */
+
+window.onscroll = function() {
+    var scrolled = window.pageYOffset || document.documentElement.scrollTop;
+    if (scrolled > 500) {
+        document.getElementById("toTop").classList.add("active");
+    } else {
+        document.getElementById("toTop").classList.remove("active");
+    }
 }
