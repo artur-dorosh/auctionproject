ed6a35f61a6ebbbe224f4499f712d933b7f2c765
artur-dorosh <50192515+artur-dorosh@users.noreply.github.com>
2019-05-06 22:30:29 +0300

    06.05.2019 22:30

--- js/js.js
+++ js/js.js
@@ -196,3 +196,139 @@ function pagination(event) {
         j++;
     }
 }
+
+/* registration */
+
+    /* signed */
+
+var sign = document.querySelector(".btn-sign");
+
+sign.addEventListener("click", function() {
+    var sign = document.getElementById("signin");
+    var signModal = document.querySelector(".signin-modal");
+    var signClose = document.querySelector(".close-btn");
+    var error = document.querySelector(".signin-error");
+
+    sign.style.display = "flex";
+    signModal.classList.add("centered");
+    signClose.onclick = function() {
+        sign.style.display = "none";
+        signModal.classList.remove("centered");
+        error.style.display = "none";
+    }
+});
+
+var users = [ {"login": "test", "pass": "123"} ];
+
+if (!localStorage.users) {
+    localStorage.users = JSON.stringify(users);
+}
+
+var enter = document.querySelector(".signin-btn");
+
+enter.addEventListener("click", function(e) {
+    e.preventDefault();
+    var users = JSON.parse(localStorage.users);
+    var login = document.querySelector(".loginin").value;
+    var password = document.querySelector(".passwordin").value;
+    var error = document.querySelector(".signin-error");
+
+    for (var i = 0; i < users.length; i++) {
+        if (users[i].login === login) {
+            if (users[i].pass === password) {
+                welcome(login);
+                break;
+            } else {
+                error.innerText = "Incorrect password";
+                error.style.display = "block";
+                break;
+            }
+        } else if (i == users.length - 1) {
+            error.innerText = "Incorrect login";
+            error.style.display = "block";
+        }
+    }
+});
+
+function welcome(name) {
+    var signModal = document.querySelector(".signin-modal");
+    var success = document.querySelector(".success");
+    var user = document.querySelector(".user");
+
+    signModal.style.display = "none";
+    success.style.display = "flex";
+    user.innerText = name;
+    localStorage.signed = "true";
+
+    var close = setTimeout(function() {
+        localStorage.user = name;
+        location.reload();
+    }, 2000);
+}
+
+function isSigned() {
+    var register = document.querySelector(".register");
+    var signed = document.querySelector(".signed-user");
+    var userWelc = document.querySelector(".signed-user_name");
+
+    if (localStorage.signed === "true") {
+        register.style.display = "none";
+        signed.style.display = "block";
+        userWelc.innerText = "Welcome, " + localStorage.user;
+    } else {
+        register.style.display = "block";
+        signed.style.display = "none";
+    }
+}
+
+isSigned();
+
+    /* logout */
+
+var logout = document.querySelector(".btn-logout");
+
+logout.addEventListener("click", function() {
+    localStorage.signed = "false";
+    location.reload();
+});
+
+    /* registrate */
+
+var regist = document.querySelector(".btn-reg");
+
+regist.addEventListener("click", function() {
+    var reg = document.getElementById("registerin");
+    var regModal = document.querySelector(".register-modal");
+    var regClose = document.querySelector(".closereg-btn");
+
+    reg.style.display = "flex";
+    regModal.classList.add("centered");
+    regClose.onclick = function() {
+        reg.style.display = "none";
+        regModal.classList.remove("centered");
+    }
+});
+
+var regEnter = document.querySelector(".register-btn");
+
+regEnter.addEventListener("click", function(e) {
+    e.preventDefault();
+    var login = document.querySelector(".loginreg").value;
+    var password = document.querySelector(".passwordreg").value;
+    var users = JSON.parse(localStorage.users);
+    var newUser = {
+        "login": login,
+        "pass": password
+    }
+    users.unshift(newUser);
+    localStorage.users = JSON.stringify(users);
+
+    var regModal = document.querySelector(".register-modal");
+    var regSuccess = document.querySelector(".successreg");
+    regModal.style.display = "none";
+    regSuccess.style.display = "flex";
+
+    var close = setTimeout(function() {
+        location.reload();
+    }, 2000);
+});
